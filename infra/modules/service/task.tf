2b5ee24f9988324cb42285e0e1c799854f6a62de
Paul Swartz <pswartz@transitops.co>
2026-05-11 13:53:56 -0400

    fix(infra): correct typo in S3 IAM policy

--- infra/modules/service/task.tf
+++ infra/modules/service/task.tf
@@ -166,7 +166,7 @@ data "aws_iam_policy_document" "task" {
   statement {
     actions = [
       "s3:GetObject",
-      "s3:GetObjecVersion"
+      "s3:GetObjectVersion"
     ]
     resources = ["${aws_s3_bucket.uploads.arn}/*"]
   }
